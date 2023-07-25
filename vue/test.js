18e78ef523a4114dc81c3e8abf1c0087b0afc825
Mohammed Saber BELLAAZIRI <99510825+msaber69@users.noreply.github.com>
2023-07-25 03:57:29 +0200

    Update test.js

--- vue/test.js
+++ vue/test.js
@@ -1,7 +1,7 @@
 // app.test.js
 
 // Import the function to be tested
-const { sum } = require('../src/app');
+const { sum } = require('vue/function.js');
 
 // Sample test cases
 test('adds 1 + 2 to equal 3', () => {
