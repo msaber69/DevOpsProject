3b92c89dcecd33865454abfff6a65b95cfabf44d
Mohammed Saber BELLAAZIRI <99510825+msaber69@users.noreply.github.com>
2023-07-25 03:42:52 +0200

    Create test.js

---
+++ vue/test.js
@@ -0,0 +1,13 @@
+// app.test.js
+
+// Import the function to be tested
+const { sum } = require('../src/app');
+
+// Sample test cases
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 3 + 5 to equal 8', () => {
+  expect(sum(3, 5)).toBe(8);
+});
